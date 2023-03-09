df23b447135ed4ea1366fe5ad5283b1e248f8b8b
Thinus <thinusvdv2001@gmail.com>
2023-03-09 17:08:08 +0200

    Fix weigth_kg values

--- data.sql
+++ data.sql
@@ -4,10 +4,10 @@ INSERT INTO animals (id, name, date_of_bith, escape_attempts, neutered, weigth_k
 INSERT INTO animals (id, name, date_of_bith, escape_attempts, neutered, weigth_kg) VALUES (2, "Gabumon", "2018-11-15", 2, True, 8kg);
 INSERT INTO animals (id, name, date_of_bith, escape_attempts, neutered, weigth_kg) VALUES (3, "Pikachu", "2021-01-07", 1, False, 15.04kg);
 INSERT INTO animals (id, name, date_of_bith, escape_attempts, neutered, weigth_kg) VALUES (4, "Devimon", "2017-05-12", 5, True, 11kg);
-INSERT INTO animals (id, name, date_of_bith, escape_attempts, neutered, weigth_kg) VALUES (5,"Charmander","2020-02-08",0,False,11,NULL);
-INSERT INTO animals (id, name, date_of_bith, escape_attempts, neutered, weigth_kg) VALUES (6,"Plantmon","2021-11-15",2,True,5.7,NULL);
-INSERT INTO animals (id, name, date_of_bith, escape_attempts, neutered, weigth_kg) VALUES (7,"Squirtle","1993-04-02",3,False,12.13,NULL);
-INSERT INTO animals (id, name, date_of_bith, escape_attempts, neutered, weigth_kg) VALUES (8,"Angemon","2005-06-12",1,True,45,NULL);
+INSERT INTO animals (id, name, date_of_bith, escape_attempts, neutered, weigth_kg) VALUES (5,"Charmander","2020-02-08",0,False,-11,NULL);
+INSERT INTO animals (id, name, date_of_bith, escape_attempts, neutered, weigth_kg) VALUES (6,"Plantmon","2021-11-15",2,True,-5.7,NULL);
+INSERT INTO animals (id, name, date_of_bith, escape_attempts, neutered, weigth_kg) VALUES (7,"Squirtle","1993-04-02",3,False,-12.13,NULL);
+INSERT INTO animals (id, name, date_of_bith, escape_attempts, neutered, weigth_kg) VALUES (8,"Angemon","2005-06-12",1,True,-45,NULL);
 INSERT INTO animals (id, name, date_of_bith, escape_attempts, neutered, weigth_kg) VALUES (9,"Boarmon","2005-06-07",7,True,20.4,NULL);
 INSERT INTO animals (id, name, date_of_bith, escape_attempts, neutered, weigth_kg) VALUES (10,"Blossom","1998-10-13",3,True,17,NULL);
 INSERT INTO animals (id, name, date_of_bith, escape_attempts, neutered, weigth_kg) VALUES (11,"Ditto","2022-05-14",4,True,22,NULL);
