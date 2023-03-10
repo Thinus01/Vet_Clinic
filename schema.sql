75e3aff45ec5ee664265f086e0b54923ad8d46ef
Thinus <thinusvdv2001@gmail.com>
2023-03-10 17:36:28 +0200

    Query multiple tables

--- schema.sql
+++ schema.sql
@@ -4,17 +4,46 @@
 
 CREATE TABLE IF NOT EXISTS public.animals
 (
-    id integer NOT NULL,
-    name character varying COLLATE pg_catalog."default",
-    date_of_birth date,
-    escape_attempts integer,
-    neutered boolean,
-    weight_kg numeric,
-    species character varying COLLATE pg_catalog."default",
-    CONSTRAINT vet_clinic_pkey PRIMARY KEY (id)
+  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY ,
+  name  VARCHAR,
+  date_of_birth date,
+  escape_attempts integer,
+  neutered  boolean,
+  weight_kg numeric
 )
 
 TABLESPACE pg_default;
 
 ALTER TABLE IF EXISTS public.animals
     OWNER to postgres;
+    
+ALTER TABLE animals ADD COLUMN species VARCHAR;
+
+CREATE TABLE owners (
+id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY ,
+	full_name VARCHAR(70),
+	age        INT
+);
+
+CREATE TABLE species (
+id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY ,
+	name VARCHAR(70)
+);
+
+ALTER TABLE animals DROP COLUMN species;
+
+ALTER TABLE animals ADD COLUMN species_id INT;
+
+ALTER TABLE animals
+ADD CONSTRAINT fk_animals
+FOREIGN KEY(species_id) 
+REFERENCES species(id)
+ON DELETE CASCADE;
+
+ALTER TABLE animals ADD COLUMN owner_id INT;
+
+ALTER TABLE animals  
+ADD CONSTRAINT fk_owners
+FOREIGN KEY(owner_id)
+REFERENCES owners(id)
+ON DELETE CASCADE;
